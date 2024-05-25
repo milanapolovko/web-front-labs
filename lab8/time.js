4398719a00b95161e836f40de710f44ef6f29eb2
Половко Милана <milana.polovko03@gmail.com>
2024-05-25 16:02:38 +0700

    Аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -1,11 +1,18 @@
 function showTime(){
     let today=new Date();
     let currenTime=today.toLocaleTimeString('ru-Ru');
-    document.getElementById('time').innerHTML=currenTime;
+    document.getElementById('time1').innerHTML=currenTime;
 
     let seconds=today.getSeconds();
-    document.getElementById('time1').innerHTML=seconds;
+    document.getElementById('time2').innerHTML=seconds;
     
 }
 
+function Clock(){
+    document.getElementById('time').style.transform=`rotate(${new Date().getSeconds()*6-180}deg) translate(0,50px)`;
+   
+}
 setInterval(showTime,1000);
+setInterval(Clock,1000);
+
+
