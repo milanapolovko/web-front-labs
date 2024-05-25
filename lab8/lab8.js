21d9fcca730f5cae54e236e91d5337de29e8a639
Половко Милана <milana.polovko03@gmail.com>
2024-05-25 13:53:05 +0700

    День недели

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -7,15 +7,16 @@ function showDate(){
     let outAR=document.getElementById('current-dateAR');
     
 
-    let year=document.getElementById('year');
-    let month=document.getElementById('month');
-    let date=document.getElementById('date');
-    let weekDay=document.getElementById('weekDay');
+    let year=document.getElementById('current-year');
+    let month=document.getElementById('current-month');
+    let date=document.getElementById('current-date');
+    let weekDay=document.getElementById('current-weekDay');
+
     let today=new Date();
 
     const weekDays=["Воскресенье","Понедельник","Вторник","Среда","Четверг","Пятница","Суббота"];
     let dayNumber=today.getDay();
-
+    
     outRU.innerHTML='Дата и время для русской локали: '+today.toLocaleString('ru-RU');
     outSP.innerHTML='Дата и время для сербской локали: '+today.toLocaleString('sr-SP');
     outZH.innerHTML='Дата и время для китайской локали: '+today.toLocaleString('zh-CN');
@@ -28,4 +29,18 @@ function showDate(){
     date.innerHTML='Текущая дата: '+today.getDate();
     weekDay.innerHTML='День недели: '+ weekDays[dayNumber];
 }
+function showDayofWeek(){
+    let inputYear=document.getElementById('year');
+    let inputMonth=document.getElementById('month');
+    let inputDate=document.getElementById('date');
 
+    let a=parseInt(inputYear.value);
+    let b=parseInt(inputMonth.value);
+    let c=parseInt(inputDate.value);
+    let out=new Date(a,b,c);
+    const weekDays=["Воскресенье","Понедельник","Вторник","Среда","Четверг","Пятница","Суббота"];
+    let Day=out.getDay();
+    let inputDay=document.getElementById('day');
+    if(a!="" &&  a>0 && b!="" && b>0  && c!="" && c>0){
+        inputDay.innerHTML=weekDays[Day];}
+}
